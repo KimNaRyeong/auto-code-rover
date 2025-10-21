3b46514a03408700d337701bb969d3cf87596b82
KimNaRyeong <kimnal1234@kaist.ac.kr>
2025-10-21 07:45:24 +0000

    generating graphs

--- atropos/data_generation.py
+++ atropos/data_generation.py
@@ -2,10 +2,12 @@ import os
 import json
 import argparse
 import copy
+import ast
 import matplotlib.pyplot as plt
-import numpy as np
+import networkx as nx
 from collections import defaultdict
 from tqdm import tqdm
+from torch_geometric.utils import from_networkx
 
 class Data_generater():
     def __init__(self, repetition, nhot = True, including_answer = True, one_arg_vector_size = False):
@@ -17,32 +19,30 @@ class Data_generater():
         if nhot:
             self.ks = list(range(1, 11))
             if self.including_answer:
-                self.ks.append(15)
-            else:
                 self.ks.extend([15, 16])
+            else:
+                self.ks.append(15)
         else:
             self.ks = list(range(1, 11))
             if self.including_answer:
-                self.ks.append(15)
+                self.ks.extend([15, 20, 38])
             else:
-                self.ks.extend([15, 32])
-
-        self.max_length_for_each_traj = None
+                self.ks.extend([15, 20, 37])
 
         task_list_file = './sampled_tasks.txt'
         with open(task_list_file, 'r') as f:
             self.task_list = f.read().splitlines()
-        self.task_list = ['astropy__astropy-6938']
+        # self.task_list = ['astropy__astropy-6938']
         # self.task_list = ['django__django-17066']
         # self.task_list = ['astropy__astropy-6938', 'django__django-17066']
         self.trajs_dict = self.extract_trajs_from_logs()
         self.reasoning_paths_dict = self.generate_reasoning_paths_dict()
 
-        print(self.trajs_dict)
-        print()
-        print(self.reasoning_paths_dict)
-        print(len(self.trajs_dict['astropy__astropy-6938'][1]))
-        print(len(self.reasoning_paths_dict['astropy__astropy-6938'][0]))
+        # print(self.trajs_dict)
+        # print()
+        # print(self.reasoning_paths_dict)
+        # print(len(self.trajs_dict['astropy__astropy-6938'][1]))
+        # print(len(self.reasoning_paths_dict['astropy__astropy-6938'][0]))
         self.args_dict = self.get_args_dict_for_k()
         self.arg_vector_size_dict = self.get_arg_vector_size()
         self.label_dict = self.get_labels_dict()
@@ -50,7 +50,6 @@ class Data_generater():
     def extract_trajs_from_logs(self):
         trajs_dict = defaultdict(dict)
 
-        print("Collecting the reasoning trajectories...")
         for task in tqdm(self.task_list):
             for i in range(1, self.repetition+1):
 
@@ -104,7 +103,7 @@ class Data_generater():
                             if fl["class_name"]:
                                 answer.add(fl["class_name"])
                             if fl["method_name"]:
-                                answer.add(fl["rel_file_path"])
+                                answer.add(fl["method_name"])
                             answer_list.append(list(answer))
                         
                         if self.nhot:
@@ -120,43 +119,26 @@ class Data_generater():
         args_dict = defaultdict(lambda: defaultdict(set))
 
         for task, reasoning_paths in self.reasoning_paths_dict.items():
-            for i, path in enumerate(reasoning_paths):
-                for reasoning_step in path:
+            for idx, path in enumerate(reasoning_paths):
+                for i, reasoning_step in enumerate(path):
                     if self.nhot:
-                        다시짜기
-
-
-
-                        for function_call in reasoning_step:
-                            for k in range(self.ks[i], self.ks[-1]):
-                                args_dict[task][k] = args_dict[task][k].union(set(function_call["arguments"].values()))
-
+                        for func_call in reasoning_step:
+                            for k in self.ks:
+                                if i < k:
+                                    if "arguments" in func_call.keys():
+                                        args_dict[task][k] = args_dict[task][k].union(set(func_call["arguments"].values()))
+                                    elif "answers" in func_call.keys():
+                                        for one_fl in func_call["answers"]:
+                                            args_dict[task][k] = args_dict[task][k].union(set(one_fl))
+                    
                     else:
-                        if reasoning_step:
-                            for function_call in reasoning_step:
-                                for k in self.ks:
-                                    if num_fc < k:
-                                        args_dict[task][k] = args_dict[task][k].union(set(function_call["arguments"].values()))
-                                num_fc += 1
-                        else:
-                            num_fc += 1
-                
-                if self.including_answer:
-                    fl_result_file = f'./fl_results/filtered_fl_result_{idx}.json'
-                    with open(fl_result_file, 'r') as f:
-                        fl_results_dict = json.load(f)
-
-                    for k in self.ks:
-                        if num_fc < k:
-                            for one_fl in fl_results_dict[task]:
-                                if one_fl["rel_file_path"]:
-                                    args_dict[task][k].add(one_fl["rel_file_path"])
-                                if one_fl["class_name"]:
-                                    args_dict[task][k].add(one_fl["class_name"])
-                                if one_fl["method_name"]:
-                                    args_dict[task][k].add(one_fl["method_name"])
-                                
-
+                        for k in self.ks:
+                            if reasoning_step != [] and i < k:
+                                if "arguments" in reasoning_step.keys():
+                                    args_dict[task][k] = args_dict[task][k].union(set(reasoning_step["arguments"].values()))
+                                elif "answers" in reasoning_step.keys():
+                                    for one_fl in reasoning_step["answers"]:
+                                        args_dict[task][k] = args_dict[task][k].union(set(one_fl))
 
         return args_dict
     
@@ -193,8 +175,21 @@ class Data_generater():
                     max_arg_size = len(arg_for_k[k])
             arg_vector_size_dict[k] = max_arg_size
         return arg_vector_size_dict
+    
+    def save_graph_image(self, graph, filename):
+        plt.figure(figsize=(12, 12))
+        pos = nx.spring_layout(graph)
+        nx.draw(graph, pos, with_labels=False, node_size=700, node_color='lightblue', font_size=10, font_weight='bold')
 
-    def generate_LIG(self, generate_S = True, generate_F = True, generate_FA = True):
+        edge_labels = nx.get_edge_attributes(graph, "weight")
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_size=9)
+
+        plt.savefig(filename, format='png')
+        plt.close()
+
+
+
+    def generate_LIG_for_all_k(self, generate_S = True, generate_F = True, generate_FA = True):
         def add_weight_edge(G, u, v, weight=1):
             if G.has_edge(u, v):
                 G[u][v]['weight'] += 1
@@ -205,46 +200,172 @@ class Data_generater():
         dataset_F = []
         dataset_FA = []
 
-        for task, trajs_with_idx in self.reasoning_paths_dict.items():
-            pass
+        # for k in self.ks:
+        print("Generating the graphs for all tasks and ks")
+        for k in [37]:
+            for task in tqdm(self.task_list):
+                graph = nx.DiGraph()
+                for _, rp in enumerate(self.reasoning_paths_dict[task]):
+                    if not rp:
+                        graph.add_node('[]')
+                        continue
+                    if self.nhot and rp[0] and 'answers' in rp[0][0].keys():
+                        for fl in rp[0][0]['answers']:
+                            graph.add_node(str(fl))
+                    else:
+                        graph.add_node(str(rp[0]))
 
+                for _, rp in enumerate(self.reasoning_paths_dict[task]):
+                    if not rp:
+                        continue
+                    for i, rs in enumerate(rp[1:]):
+                        if i + 1 < k:
+                            if self.nhot:
+                                if self.including_answer and rs and 'answers' in rs[0].keys():
+                                    for fl in rs[0]['answers']:
+                                        if not graph.has_node(str(fl)):
+                                            graph.add_node(str(fl))
+
+                                        add_weight_edge(graph, str(rp[i]), str(fl))
+                                else:
+                                    if not graph.has_node(str(rs)):
+                                        graph.add_node(str(rs))
+                                    add_weight_edge(graph, str(rp[i]), str(rs))
+                            else:
+                                if self.including_answer and rs and 'answers' in rs.keys():
+                                    for fl in rs['answers']:
+                                        if not graph.has_node(str(fl)):
+                                            graph.add_node(str(fl))
+
+                                        add_weight_edge(graph, str(rp[i]), str(fl))
+                                else:
+                                    if not graph.has_node(str(rs)):
+                                        graph.add_node(str(rs))
+                                    add_weight_edge(graph, str(rp[i]), str(rs))
+
+                            # for node in graph.nodes():
+                            #     print(node)
+                            # for edge in graph.edges():
+                            #     print(edge)
+
+
+                if self.nhot:
+                    if self.including_answer:
+                        graph_dir = f'./graphs/lig/nhot/including_answer/{k}'
+                    else:
+                        graph_dir = f'./graphs/lig/nhot/no_answer/{k}'
+                else:
+                    if self.including_answer:
+                        graph_dir = f'./graphs/lig/onehot/including_answer/{k}'
+                    else:
+                        graph_dir = f'./graphs/lig/onehot/no_answer/{k}'
+
+                os.makedirs(graph_dir, exist_ok=True)
+                try:
+                    self.save_graph_image(graph, os.path.join(graph_dir, f'{task}.png'))
+                except:
+                    print(f"Failed to save the graph: {os.path.join(graph_dir, f'{task}.png')}")
+            
+                # S_data = from_networkx(graph)
+                # F_data = from_networkx(graph)
+                # FA_data = from_networkx(graph)
+
+                # S_node_x = []
+                # F_node_x = []
+                # FA_node_x = []
+
+                # for node_str in graph.nodes():
+                #     func_vector = [0] * (len(self.function_types) + 1)
+                #     if self.nhot:
+                #         if 'answers' in node_str:
+
+                    # else:
+                    #     if "'answers'" in node_str and self.including_answer:
+                    #         pass
+                    #     else:
+                    #         if node_str == '[]':
+                    #             func_vector[-1] = 1
+                    #         else:
+                    #             node = ast.literal_eval(node_str)
+
+                    
+
+                
+
+
+                    
+
+            
+        
+
+
+                
+
+                
+
+                
+        test_graph_list = []
+        test_graph = nx.DiGraph()
+        test_graph.add_node('a')
+        test_graph.add_node('b')
+        add_weight_edge(test_graph, 'a', 'b')
+
+        test_graph_list.append(test_graph)
+
+        test_graph = copy.deepcopy(test_graph)
+        test_graph.add_node('c')
+        test_graph.add_node(str([]))
+        add_weight_edge(test_graph, 'b', 'c')
+
+        test_graph_list.append(test_graph)
+        
+        for graph in test_graph_list:
+            print(graph.nodes())
+        print(test_graph.nodes())
 
         
+
+
+            
 
 
     def draw_length_distribution_graphs(self):
-        length_dict_nhot = defaultdict(int)
-        length_dict_onehot = defaultdict(int)
+        length_dict = defaultdict(int)
         
         for task_name, trajs in self.reasoning_paths_dict.items():
             for traj in trajs:
-                num_fc = 0
-                for reasoning_step in traj:
-                    num_fc += len(reasoning_step)
-                    if not reasoning_step:
-                        num_fc += 1
-                length_dict_nhot[len(traj)] += 1
-                length_dict_onehot[num_fc] += 1
-        if self.nhot:
-            self.max_length_for_each_traj = max(length_dict_nhot.keys())
-        else:
-            self.max_length_for_each_traj = max(length_dict_onehot.keys())
-
-        print(max(length_dict_nhot.keys()))
-        print(max(length_dict_onehot.keys()))
+                length_dict[len(traj)] += 1
+        max_length = max(length_dict.keys())
+        print(max_length)
         
         # Draw the length distibution graphs
-        plt.bar(list(length_dict_nhot.keys()), list(length_dict_nhot.values()), color='skyblue')
-        plt.title('The length distribution of each traj. (nhot ver.)')
-        plt.xlabel('Length')
-        plt.grid(axis='y')
-        plt.savefig('./graphs/length_distribution_nhot')
+        plt.bar(list(length_dict.keys()), list(length_dict.values()), color='skyblue')
 
-        plt.bar(list(length_dict_onehot.keys()), list(length_dict_onehot.values()), color='skyblue')
-        plt.title('The length distribution of each traj. (onehot ver.)')
+        title = 'The length distribution of each traj.'
+        if self.nhot:
+            title += ' (nhot,'
+        else:
+            title += ' (onehot,'
+        if self.including_answer:
+            title += ' answer)'
+        else:
+            title += ' no answer)'
+
+        plt.title(title)
         plt.xlabel('Length')
         plt.grid(axis='y')
-        plt.savefig('./graphs/length_distribution_onehot')
+
+        fig_name = 'length_distribution'
+        if self.nhot:
+            fig_name += '_nhot'
+        else:
+            fig_name += '_onehot'
+        if self.including_answer:
+            fig_name += '_answer'
+        else:
+            fig_name += '_no_answer'
+        plt.savefig(f"./graphs/{fig_name}")
+
 
     def generate_vector_for_all_tasks(self):
         args_dict = defaultdict(set)
@@ -331,6 +452,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data_generater = Data_generater(args.repetition, args.nhot, args.answer, args.one_arg_vector_size)
+
+    # data_generater.draw_length_distribution_graphs()
+
+
+    #=================================================================
     # reasoning_paths_dict = data_generater.get_reasoning_paths_for_all_tasks()
     # with open('./reasoning_paths.json', 'w') as f:
     #     json.dump(reasoning_paths_dict, f, indent=4)
@@ -350,6 +476,9 @@ if __name__ == '__main__':
     # examine_tool_call_layers() 
     # print(len(data_generater.label_dict.values()))
     # print(sum(data_generater.label_dict.values()))
+    data_generater.generate_LIG_for_all_k()
+
+
 
     
 
